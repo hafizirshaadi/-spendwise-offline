8a608d34f4cb329d0911d563e927c70b2628405c
hafizirshaadi <muhammadhafizirshaadi14@gmail.com>
2026-08-05 11:00:19 +0800

    Cache updated SpendWise offline assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='spendwise-offline-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+const CACHE='spendwise-offline-v2';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.full.min.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
