be3f828ec5f3529bb53ac7c2f0045d633aed7a15
hafizirshaadi <muhammadhafizirshaadi14@gmail.com>
2026-08-05 14:18:25 +0800

    Refresh offline cache for layout update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='spendwise-offline-v11';
+const CACHE='spendwise-offline-v12';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.full.min.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
